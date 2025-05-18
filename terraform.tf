3355054aca3bf3f83d741f2ed59939ed7572e5f1
MarianReda <139588370+MarianReda@users.noreply.github.com>
2025-05-18 16:39:18 +0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,15 +1,50 @@
 provider "aws" {
   region = "us-east-2"  # Change to your preferred region
 }
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-terraform-state-bucket"
+  force_destroy = true
 
-# Backend config (place in a separate file or init block)
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-state-bucket"
-    key    = "ec2/keypair-demo/terraform.tfstate"
-    region = "us-east-1"
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "dev"
   }
 }
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "dev"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
+
 
 # Create a VPC
 resource "aws_vpc" "main" {
